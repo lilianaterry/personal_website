9ae92108d7f3c65167e060dc19ce90711db16a7d
Liliana Terry <lilianaterry@Lilianas-MacBook-Pro.local>
2023-10-29 11:07:38 -0700

    added cursor animation

--- personal-website-react-app/src/App.js
+++ personal-website-react-app/src/App.js
@@ -8,6 +8,11 @@ import Process from "./components/Process";
 import Contact from "./components/Contact";
 import Contribute from "./components/Contribute";
 
+import { fairyDustCursor } from "cursor-effects";
+new fairyDustCursor({
+  colors: ["#E35A20", "#FCAD8B", "#C9ADA1", "#C99781"],
+});
+
 function App() {
   return (
     <Router>
